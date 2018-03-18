90b5e58ceb1dc8f80dfb545747682cd6a8e81e17
Ji Yim <gsooyim@gmail.com>
2018-03-18 01:13:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Jane"
-var height = "60"
+var height = 60
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) + 1)
+console.log(height + 1)
